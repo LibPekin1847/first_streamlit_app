09253fd175c1311608d4aa562dd322c259c507f6
LibPekin1847 <115427737+LibPekin1847@users.noreply.github.com>
2022-10-15 16:29:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+# New session to display fruityvice apt response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 steaamlit.text(fruityvice_reponse)
+
+# New session to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
